8a20103e46361cf4f671a21cab6567141596d3f3
svante karlsson <saka@csi.se>
2014-11-26 16:21:36 +0100

    updated build settings

--- rebuild_win64_vc12.bat
+++ rebuild_win64_vc12.bat
@@ -1,22 +1,19 @@
 @ECHO OFF
 
-rmdir /S /Q bin
-rmdir /S /Q lib
-rmdir /S /Q win_build32
+rmdir /S /Q bin\x64
+rmdir /S /Q lib\x64
 rmdir /S /Q win_build64
 
-set BOOST_ROOT=%CD%\..\boost_1_55_0
+REM set BOOST_ROOT=%CD%\..\boost_1_57_0
 
 ECHO ===== CMake for 64-bit ======
 call "C:\Program Files (x86)\Microsoft Visual Studio 12.0\VC\vcvarsall.bat" amd64
 mkdir win_build64 
 cd win_build64
-del CMakeCache.txt
-
 
 cmake -G "Visual Studio 12 Win64" ..
-devenv ALL_BUILD.vcxproj /Build "Debug|x64"
-devenv ALL_BUILD.vcxproj /Build "Release|x64"
+msbuild ALL_BUILD.vcxproj /p:Configuration=Debug /p:Platform=x64
+msbuild ALL_BUILD.vcxproj /p:Configuration=Release /p:Platform=x64
 cd ..
 
 
